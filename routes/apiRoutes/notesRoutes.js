534b473b3214b3d227b9e252edda49e46fa29374
Nuo <codypham5@gmail.com>
2022-11-16 17:35:53 -0500

    Added routes.

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const { createNewNote, deleteNote } = require("../../lib/notes");
+let { notesArray } = require("../../db/notes");
+
+//Notes are available at api/notes in JSON
+router.get("/notes", (req, res) => {
+  let results = notesArray;
+  res.json(results);
+});
+
+router.post("/notes", (req, res) => {
+  //Set ID based on what the next index of the array will be
+  if (notesArray) {
+    req.body.id = notesArray.length.toString();
+  } else {
+    req.body.id = 0;
+  }
+  res.json(createNewNote(req.body, notesArray));
+});
+
+router.delete("/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  notesArray = await deleteNote(id, notesArray);
+  res.json(notesArray);
+});
+
+module.exports = router;
