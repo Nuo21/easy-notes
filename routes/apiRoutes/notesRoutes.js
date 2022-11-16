37d33c9d95afc90174d184e71b63dbc9e5693656
Nuo <codypham5@gmail.com>
2022-11-16 17:58:34 -0500

    Added lib folder and fixed paths

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 const { createNewNote, deleteNote } = require("../../lib/notes");
-let { notesArray } = require("../../db/notes");
+let { notesArray } = require("../../db/db.json");
 
 //Notes are available at api/notes in JSON
-router.get("/notes", (req, res) => {
+router.get("/db", (req, res) => {
   let results = notesArray;
   res.json(results);
 });
 
-router.post("/notes", (req, res) => {
+router.post("/db", (req, res) => {
   //Set ID based on what the next index of the array will be
   if (notesArray) {
     req.body.id = notesArray.length.toString();
@@ -18,7 +18,7 @@ router.post("/notes", (req, res) => {
   res.json(createNewNote(req.body, notesArray));
 });
 
-router.delete("/notes/:id", async (req, res) => {
+router.delete("/db/:id", async (req, res) => {
   const { id } = req.params;
   notesArray = await deleteNote(id, notesArray);
   res.json(notesArray);
